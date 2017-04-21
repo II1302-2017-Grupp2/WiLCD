b2ced2718d3255df27743a7309fef78f97326563
Teo Klestrup Röijezon <teo@nullable.se>
2017-04-21 13:09:13 +0200

    Lade till en READY-pin för när chippet har startat

--- Prototyp-teo/Src/gpio.c
+++ Prototyp-teo/Src/gpio.c
@@ -63,10 +63,10 @@ void MX_GPIO_Init(void)
   __HAL_RCC_GPIOB_CLK_ENABLE();
 
   /*Configure GPIO pin Output Level */
-  HAL_GPIO_WritePin(GPIOE, ESP_RESET_Pin|ESP_ENABLE_Pin|ESP_GPIO0_Pin, GPIO_PIN_RESET);
+  HAL_GPIO_WritePin(GPIOE, ESP_RESET_Pin|ESP_ENABLE_Pin|ESP_GPIO0_Pin|READY_LED_Pin, GPIO_PIN_RESET);
 
-  /*Configure GPIO pins : PEPin PEPin PEPin */
-  GPIO_InitStruct.Pin = ESP_RESET_Pin|ESP_ENABLE_Pin|ESP_GPIO0_Pin;
+  /*Configure GPIO pins : PEPin PEPin PEPin PEPin */
+  GPIO_InitStruct.Pin = ESP_RESET_Pin|ESP_ENABLE_Pin|ESP_GPIO0_Pin|READY_LED_Pin;
   GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
   GPIO_InitStruct.Pull = GPIO_NOPULL;
   GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
