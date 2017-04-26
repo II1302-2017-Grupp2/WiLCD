4bce752826eafb92ed5a3fa3bfc15a35daa23ac9
Teo Klestrup Röijezon <teo@nullable.se>
2017-04-26 02:24:54 +0200

    Tillfälligt: Skicka meddelande per nytt meddelande istf att polla varje sekund

--- wilcd-ui/app/services/MessageUpdaterDatabase.scala
+++ wilcd-ui/app/services/MessageUpdaterDatabase.scala
@@ -1,17 +1,19 @@
 package services
 
-import javax.inject.Inject
+import javax.inject.{Inject, Named}
 
+import actors.DbMessageFetcher
+import akka.actor.ActorRef
 import models.{Messages, PgProfile}
 import play.api.db.slick.{DatabaseConfigProvider, HasDatabaseConfigProvider}
 import play.api.libs.concurrent.Execution.Implicits.defaultContext
 
 import scala.concurrent.Future
 
-class MessageUpdaterDatabase @Inject()(val dbConfigProvider: DatabaseConfigProvider) extends MessageUpdater with HasDatabaseConfigProvider[PgProfile] {
+class MessageUpdaterDatabase @Inject()(@Named("db-message-fetcher") dbMessageFetcher: ActorRef, val dbConfigProvider: DatabaseConfigProvider) extends MessageUpdater with HasDatabaseConfigProvider[PgProfile] {
   override def isDeviceConnected: Future[Boolean] = Future.successful(true)
 
-  override def setMessage(msg: String): Future[Unit] = db.run(Messages.create(msg))
+  override def setMessage(msg: String): Future[Unit] = db.run(Messages.create(msg)).map(_ => dbMessageFetcher ! DbMessageFetcher.Refresh)
 
   override def getMessage: Future[String] = db.run(Messages.currentMessage.map(_.map(_.value.message).getOrElse("")))
 }
