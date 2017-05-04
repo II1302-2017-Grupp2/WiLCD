6a48b61e109f7476fdebe058b43afc06e00778c7
Teo Klestrup Röijezon <teo@nullable.se>
2017-05-04 16:18:08 +0200

    Började fixa lite mer tester

--- wilcd-ui/test/actors/TcpDisplayUpdaterSpec.scala
+++ wilcd-ui/test/actors/TcpDisplayUpdaterSpec.scala
@@ -1,34 +1,36 @@
 package actors
 
 import java.net.{InetSocketAddress, Socket}
+import java.time.ZoneId
 import java.util.Scanner
-import javax.inject.Qualifier
 
-import akka.actor.ActorRef
-import controllers.OurPlaySpec
-import models.{Id, User}
-import org.scalatestplus.play.guice.GuiceOneAppPerTest
-import play.api.inject.{BindingKey, QualifierAnnotation}
-import services.MessageUpdaterNetwork
+import controllers.{DbOneAppPerTest, OurPlaySpec}
+import models.User
+import org.scalatest.concurrent.Eventually
+import services.{MessageUpdater, UserService}
 
 import scala.concurrent.Await
-import scala.concurrent.duration.Duration
-import org.scalatest.concurrent.Eventually
+import scala.concurrent.duration._
 
-class TcpDisplayUpdaterSpec extends OurPlaySpec with GuiceOneAppPerTest with Eventually {
+class TcpDisplayUpdaterSpec extends OurPlaySpec with DbOneAppPerTest with Eventually {
   "TcpDisplayUpdater ! Update" should {
     "Send the updated message to all clients" in {
-      val updater = app.injector.instanceOf[MessageUpdaterNetwork]
+      val updater = app.injector.instanceOf[MessageUpdater]
+      val users = app.injector.instanceOf[UserService]
+      val user = Await.result(users.create(User("a@a", ZoneId.of("UTC")), ""), 1.second).get
       val socket = new Socket()
       socket.setSoTimeout(1000)
       socket.connect(new InetSocketAddress("127.0.0.1", 9797))
       eventually {
         Await.result(updater.isDeviceConnected, Duration.Inf) mustBe true
       }
-      updater.setMessage(Id[User](-1), "blah")
+      Await.result(updater.setMessage(user, "blah"), 1.second)
       val inputStream = socket.getInputStream
       val scanner = new Scanner(inputStream)
-      scanner.nextLine() mustBe "blah"
+      eventually {
+        val line = scanner.nextLine()
+        line mustBe "blah"
+      }
     }
   }
 }
