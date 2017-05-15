80d9375f96bf8c55de53f84e4ac40a71e4d5ff2d
Teo Klestrup Röijezon <teo@nullable.se>
2017-05-15 15:34:00 +0200

    Krascha inte om inga dokument har ändrats

--- travis-deploy.sh
+++ travis-deploy.sh
@@ -4,7 +4,7 @@ pushd docs
 ./konvertera.sh
 popd
 git add .
-git commit -m "Travis: konverterade filer [skip ci]"
+git commit -m "Travis: konverterade filer [skip ci]" || true
 git checkout -B master
 git remote set-url origin git@github.com:II1302-2017-Grupp2/WiLCD.git
 git push
