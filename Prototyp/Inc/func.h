48ac3883b355374841c037ccab7d4b82eab7c7d4
Teo Klestrup Röijezon <teo@nullable.se>
2017-05-17 12:50:56 +0200

    Organiserade om MCU-kod

--- Prototyp/Inc/func.h
+++
@@ -1,6 +0,0 @@
-#ifndef __FUNC_H
-#define __FUNC_H
-
-void Blink(void);
-
-#endif
--- PrototypHenrik/WilcdUI/Inc/font.h
+++
@@ -1,9 +0,0 @@
-#ifndef __font_H
-#define __font_H
-
-#include <stddef.h>
-#include <stdint.h>
-
-extern uint8_t *font[];
-
-#endif /* __font_H */
