13c3ee725efdcf1b4ac67eb6d5b488f8681771c6
Teo Klestrup Röijezon <teo@nullable.se>
2017-05-17 21:21:17 +0200

    Uppdaterade diverse skript med den nya mappstrukturen

--- travis-deploy.sh
+++ travis-deploy.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 set -eou pipefail
-pushd docs
+pushd Dokument
 ./konvertera.sh
 popd
 git add .
-git commit -m "Travis: konverterade filer [skip ci]" || true
+git commit -m "Travis: konverterade dokument [skip ci]" || true
 git checkout -B master
 git remote set-url origin git@github.com:II1302-2017-Grupp2/WiLCD.git
 git push
