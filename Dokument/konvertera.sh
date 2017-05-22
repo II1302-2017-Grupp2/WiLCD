66b577ce6eeb461d8fe81ef03591286d4918c084
Teo Klestrup Röijezon <teo@nullable.se>
2017-05-22 18:07:17 +0200

    Stängde av autokonvertering av org till pdf

--- Dokument/konvertera.sh
+++ Dokument/konvertera.sh
@@ -11,23 +11,23 @@ do
     fi
 done
 
-for file in **/*.{org,tex}
-do
-    echo Converting $file to pdf
-    newSum="$(sha256sum "$file")"
-    oldSum=""
-    if [ -e "$file.hash" ]
-    then
-        oldSum="$(cat "$file.hash")"
-    fi
-    if [ "$newSum" = "$oldSum" ]
-    then
-        echo Unchanged, skipping
-        continue
-    fi
-    echo "$newSum" > "$file.hash"
-    pushd "$(dirname "$file")" > /dev/null
-    filebase="$(basename "$file")"
-    pandoc --to=latex "--output=${filebase%.*}.pdf" "$filebase"
-    popd > /dev/null
-done
+# for file in **/*.{org,tex}
+# do
+#     echo Converting $file to pdf
+#     newSum="$(sha256sum "$file")"
+#     oldSum=""
+#     if [ -e "$file.hash" ]
+#     then
+#         oldSum="$(cat "$file.hash")"
+#     fi
+#     if [ "$newSum" = "$oldSum" ]
+#     then
+#         echo Unchanged, skipping
+#         continue
+#     fi
+#     echo "$newSum" > "$file.hash"
+#     pushd "$(dirname "$file")" > /dev/null
+#     filebase="$(basename "$file")"
+#     pandoc --to=latex "--output=${filebase%.*}.pdf" "$filebase"
+#     popd > /dev/null
+# done
